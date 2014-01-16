81367b2e8bc34c89035e80160c391f7760854e25
Duy Lam <duylam@kms-technology.com>
2014-01-16 14:45:57 +0700

    change selected tab when navigating

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,18 +1,32 @@
 (function() {
   angular
-    .module("mwp", [ "ngRoute" ])
+    .module( "mwpControllers", [])
+    .controller( "homeController", function() {})
+    .controller( "newsController", function() {})
+    .controller( "phoneController", function() {});
+  angular
+    .module("mwp", [ "ngRoute", "mwpControllers" ])
     .config([ "$routeProvider", "$locationProvider", function($routeProvider, $locationProvider) {
       $locationProvider.html5Mode(true);
       $routeProvider
         .when( "/", { 
-          templateUrl: "home.html"
+          templateUrl: "home.html",
+          controller: "homeController"
         })
         .when( "/news", { 
-          templateUrl: "news.html"
+          templateUrl: "news.html",
+          controller: "newsController"
         })
         .when( "/phones", { 
-          templateUrl: "phones.html"
+          templateUrl: "phones.html",
+          controller: "phoneController"
         })
         .otherwise({ redirectTo: "/" });
-    }]);
+    }])
+    .run(function($rootScope) {
+      $rootScope.controllerName = "homeController";
+      $rootScope.$on("$routeChangeSuccess", function(ev, current) {   
+        $rootScope.controllerName = current.controller;
+      });  
+    });
 })();
